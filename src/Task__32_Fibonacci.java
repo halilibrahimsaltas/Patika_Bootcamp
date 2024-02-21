cc63329e6f9faf34d55661b884a11f841d2e83e9
Halil İbrahim <halil.ali.gs.1430@gmail.com>
2024-02-21 10:53:26 +0300

    Fibonacci Sayısı Bulma

--- src/Task__32_Fibonacci.java
+++ src/Task__32_Fibonacci.java
@@ -16,7 +16,7 @@ public class Task__32_Fibonacci {
         System.out.print(fib[0] + " ");
         if (number > 0) {//Sıfırdan büyük olması kontrolü
             System.out.print(fib[1] + " ");
-        for(int i=2 ; i<=number;i++){//Fibonacci sayısı için gerekli loop
+        for(int i=2 ; i<number;i++){//Fibonacci sayısı için gerekli loop
                 fib[i] = fib[i - 1] + fib[i - 2];
                 System.out.print(" " + fib[i]); // Yazdırma çıktı
         }
