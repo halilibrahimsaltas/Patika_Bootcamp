892719e433c81bd8d5e7af5b6c1c56009a1b7584
Halil İbrahim <halil.ali.gs.1430@gmail.com>
2024-03-18 10:05:31 +0300

    Try-Catch

--- CohortTask_Week_5/Try-Catch/src/Main.java
+++ CohortTask_Week_5/Try-Catch/src/Main.java
@@ -1,5 +1,25 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Try-Catch
+        Scanner input = new Scanner(System.in);
+       String [] array={"tomato","car","soccer","wrap","dungeon","solid","rescue","support","casual","unbelievable"};
+
+       while(true) {// Try-Catch loop
+
+           try {
+               System.out.print("Enter a index between 0-9: ");
+               int select = input.nextInt();
+               System.out.println(array[select]);
+
+           } catch (ArrayIndexOutOfBoundsException e) {
+               System.out.println("ArrayIndexOutOfBoundsException: Wrong index selection !!");
+               System.out.println(e.getMessage());
+
+           }
+
+       }
     }
 }
