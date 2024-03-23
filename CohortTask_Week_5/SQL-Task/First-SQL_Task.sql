9aec635b1c0d7326fa7311a5b73eef324fb81002
Halil İbrahim SALTAŞ <82754847+halilibrahimsaltas@users.noreply.github.com>
2024-03-23 11:07:34 +0300

    First
    -

---
+++ CohortTask_Week_5/SQL-Task/First-SQL_Task.sql
@@ -0,0 +1,16 @@
+--1
+SELECT title,description FROM film;
+
+--2
+SELECT * FROM film
+WHERE length > 60 AND length < 75;
+
+--3
+SELECT * FROM film
+WHERE rental_rate=0.99 AND replacement_cost=12.99 OR replacement_cost=28.99;
+
+--4
+SELECT last_name FROM customer WHERE first_name='Mary';
+
+--5
+SELECT * FROM film where NOT length >50 AND NOT rental_rate=2.99 OR rental_rate=4.99;
