3e24eb9a2439339eb8453dccad1a86846a16e250
Halil İbrahim SALTAŞ <82754847+halilibrahimsaltas@users.noreply.github.com>
2024-03-23 16:38:56 +0300

    Fourth-SQL_Task

---
+++ CohortTask_Week_5/SQL-Task/Fourth-SQL_Task/Fourth-SQL_Task.sql
@@ -0,0 +1,18 @@
+--1
+SELECT DISTINCT replacement_cost FROM film;
+
+--2
+SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+--3
+SELECT COUNT(*) FROM film
+WHERE title LIKE 'T%' AND rating='G';
+
+--4
+SELECT COUNT(*) FROM country
+WHERE country LIKE '_____'
+
+
+--5
+SELECT COUNT(*) FROM city
+WHERE city ILIKE '%r'; 
