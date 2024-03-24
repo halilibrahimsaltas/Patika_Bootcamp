b447172918ebb89e3c2c9133b754bf9b747676a8
Halil İbrahim SALTAŞ <82754847+halilibrahimsaltas@users.noreply.github.com>
2024-03-24 12:23:51 +0300

    Sixth-SQL_Task

---
+++ CohortTask_Week_5/SQL-Task/Sixth-SQL_Tasl/Sixth-SQL_Task.sql
@@ -0,0 +1,15 @@
+--1
+SELECT AVG(rental_rate) FROM film;
+
+--2
+SELECT COUNT(title) FROM film
+WHERE title LIKE 'C%';
+
+--3
+SELECT MAX(length) FROM film
+WHERE rental_rate=0.99 ;
+
+--4
+SELECT COUNT(DISTINCT replacement_cost) FROM film
+WHERE length > 150;
+
