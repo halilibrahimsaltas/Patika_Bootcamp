eca31c48214f8fdf41b48266676f26855475eab7
Halil İbrahim <halil.ali.gs.1430@gmail.com>
2024-04-07 10:07:56 +0300

    Patika

---
+++ Patika_Task_Week_7/src/view/BookingView.java
@@ -0,0 +1,26 @@
+package view;
+
+import javax.swing.*;
+
+public class BookingView {
+    private JPanel container;
+    private JLabel lbl_header;
+    private JLabel lbl_car_info;
+    private JLabel lbl_book_name;
+    private JTextField fld_book_name;
+    private JLabel lbl_km_no;
+    private JTextField fld_book_idno;
+    private JLabel lbl_book_mpno;
+    private JTextField fld_book_mpno;
+    private JLabel lbl_book_mail;
+    private JTextField fld_book_mail;
+    private JLabel lbl_book_strt_date;
+    private JTextField fld_book_strt_date;
+    private JLabel lbl_book_fnsh_date;
+    private JTextField fld_book_fnsh_date;
+    private JLabel lbl_book_prc;
+    private JTextField fld_book_prc;
+    private JLabel lbl_book_note;
+    private JTextField fld_book_note;
+    private JButton btn_book_save;
+}
