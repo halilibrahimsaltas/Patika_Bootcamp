92766781f8255408c2355d27ac9ca644d3808768
Halil İbrahim <halil.ali.gs.1430@gmail.com>
2024-04-08 16:32:21 +0300

    -

--- Patika_Task_Week_7/src/core/Db.java
+++ Patika_Task_Week_7/src/core/Db.java
@@ -1,23 +1,35 @@
 package core;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
 public class Db {
     //Singleton Design Pattern
     private  static Db instance=null;
     private Connection connection= null;
-    private  final String DB_URL = "jdbc:postgresql://localhost:5432/rentacar";
-
-    private  final String DB_USERNAME= "postgres";
-    private  final String DB_PASS ="1234";
 
     public Db() {
         try{
+            Properties props = new Properties();
+            FileInputStream in= new FileInputStream("C:\\Users\\TRON-V3\\IdeaProjects\\CohortTasks\\Patika_Task_Week_7\\database.properties");
+            props.load(in);
+            in.close();
+            String DB_URL = props.getProperty("db.url");
+            String DB_USERNAME = props.getProperty("db.username");
+            String DB_PASS = props.getProperty("db.password");
             this.connection=DriverManager.getConnection(DB_URL,DB_USERNAME,DB_PASS);
 
         }catch (SQLException e){
             System.out.println(e.getMessage());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
     public Connection getConnection(){
